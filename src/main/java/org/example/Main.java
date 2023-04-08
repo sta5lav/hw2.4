9bdde5f04ab7199f8a35e11b5d926124b13c894b
Stanislav <stdu92@mail.ru>
2023-04-08 21:49:08 +0300

    Exceptions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,41 @@
 package org.example;
 
+import org.example.exceptions.WrongLoginException;
+import org.example.exceptions.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(validateRegistration("petr", "12345Password_","12345Password_"));
+    }
 
-        System.out.println("Hello world!");
+
+    public static boolean validateRegistration(String login, String password, String confirmPassword) {
+        try {
+            registration(login, password, confirmPassword);
+            return true;
+        } catch (WrongPasswordException | WrongLoginException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // boolean onlyLatinAlphabet = string.matches("^[a-zA-Z0-9]+$");
+    public static void registration(String login, String password, String confirmPassword) throws WrongPasswordException, WrongLoginException {
+        if (login.length() > 20 || login == null) {
+            throw new WrongLoginException();
+        }
+        if (password.length() >= 20 || password == null) {
+            throw new WrongPasswordException();
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
+        if(!login.matches("^\\w+$")){
+            throw new WrongLoginException();
+        }
+        if(!password.matches("^\\w+$")){
+            throw new WrongPasswordException();
+        }
+
     }
 }
---
+++ src/main/java/org/example/exceptions/WrongLoginException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class WrongLoginException extends Exception {
+
+    public WrongLoginException() {
+        super();
+    }
+}
